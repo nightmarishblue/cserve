72af2fdf7884558aa8eb22db6aea1a5c5ae8a948
blue <midnight@nightmarish.blue>
2024-09-16 13:19:27 +0100

    Use vasprintf if building for GNU

--- src/str.c
+++ src/str.c
@@ -8,20 +8,28 @@ char* msprintf(const char* fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
-    int size = vsnprintf(NULL, 0, fmt, args);
-    va_end(args);
-    if (size == -1) return NULL;
+    #ifdef _GNU_SOURCE
+        char* str;
+        int len = vasprintf(&str, fmt, args);
+        va_end(args);
+        if (len == -1) return NULL;
+        return str;
+    #else
+        int size = vsnprintf(NULL, 0, fmt, args);
+        va_end(args);
+        if (size == -1) return NULL;
 
-    char* str = malloc((size + 1) * sizeof(char));
-    if (!str) return NULL;
+        char* str = malloc((size + 1) * sizeof(char));
+        if (!str) return NULL;
 
-    va_start(args, fmt);
-    size = vsnprintf(str, size + 1, fmt, args);
-    va_end(args);
-    if (size == -1)
-    {
-        free(str);
-        return NULL;
-    }
-    return str;
+        va_start(args, fmt);
+        size = vsnprintf(str, size + 1, fmt, args);
+        va_end(args);
+        if (size == -1)
+        {
+            free(str);
+            return NULL;
+        }
+        return str;
+    #endif
 }
